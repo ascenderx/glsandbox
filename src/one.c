4c74a02280383a7ad9c680c7025b2bda6daa6e79
James D. Downer <downer.james.256k@gmail.com>
2018-07-13 14:36:38 -0600

    Change #define to consts

--- src/one.c
+++ src/one.c
@@ -4,47 +4,47 @@
 
 #include "draw.h"
 
-#define WIN_WIDTH  640
-#define WIN_HEIGHT 480
-
-#define FPS 30
-
 int main(int argc, char ** argv) {
     GLFWwindow * window;
 
-    /* Initialize the library */
+    // Initialize the library
     if (!glfwInit())
         return 1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "GLFW Tutorial One", NULL, NULL);
+    
+    const int WIN_WIDTH  = 640;
+    const int WIN_HEIGHT = 480; 
+    const int FPS        = 60;
+    const char * title   = "GLFW Test One";
+    
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, title, NULL, NULL);
     if (!window) {
         glfwTerminate();
         return 1;
     }
 
-    /* Make the window's context current */
+    // Make the window's context current
     glfwMakeContextCurrent(window);
 
-    /* init */
+    // Initialize user code
     uInitEngine();
     uInit2DRenderer(WIN_WIDTH, WIN_HEIGHT);
 
-    /* Loop until the user closes the window */
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
-        /* Update here */
+        // Update here
         uUpdate();
 
-        /* Render here */
+        // Render here
         uRender2D();
 
-        /* Swap front and back buffers */
+        // Swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Poll for and process events
         glfwPollEvents();
 
-        /* sleep */
+        // Sleep before looping
         usleep(1000000 / FPS);
     }
 
